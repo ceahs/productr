b53a5ed06a035cea897528b0c54011f7a7ec6549
ceahs <chelsyinhabbo@gmail.com>
2022-07-05 19:31:19 +1000

    usernames setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_17_070911) do
+ActiveRecord::Schema.define(version: 2022_07_05_031227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,39 @@ ActiveRecord::Schema.define(version: 2022_06_17_070911) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "listing_news", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_listing_news_on_user_id"
+  end
+
+  create_table "listings", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "price"
+    t.boolean "sold", default: false
+    t.bigint "user_id", null: false
+    t.bigint "catagory_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["catagory_id"], name: "index_listings_on_catagory_id"
+    t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -74,4 +107,8 @@ ActiveRecord::Schema.define(version: 2022_06_17_070911) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "listing_news", "users"
+  add_foreign_key "listings", "catagories"
+  add_foreign_key "listings", "users"
+  add_foreign_key "products", "users"
 end
