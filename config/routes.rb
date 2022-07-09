171e10c135a4682b5e146a19226f6e33c98c3d6e
ceahs <chelsyinhabbo@gmail.com>
2022-07-10 04:14:54 +1000

    buy order

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :products
   devise_for :users
   root 'pages#home'
+
+  post "products/:id/order", to: "products#place_order", as: "place_order"
+
+  get 'pages/success', to: 'pages#success', as: "order_success"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
